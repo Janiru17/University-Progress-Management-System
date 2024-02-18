ef2cfcf0bfb75abf6a36535822ef6a9fc8a81f74
Janiru Wickramage <81567001+Janiru17@users.noreply.github.com>
2024-02-18 21:24:34 +0530

    Add files via upload

---
+++ Staff_MainProgram_1998814.py
@@ -0,0 +1,200 @@
+# I declare that my work contains no examples of misconduct, such as plagiarism, or collusion. 
+# Any code taken from other sources is referenced within my code solution.  
+  
+# Student ID: 20220218 | W1998814          # Date:  2023/04/20
+
+# variables
+credit_range = [0,20,40,60,80,100,120]
+credit_pass=0
+credit_defer=0
+credit_fail=0
+credit_total=0
+ans = 0
+
+file_object = 0
+#lists
+ProgressList = []
+MTrailerList = []
+RetrieverList = []
+
+ExcludedList = []
+i = []
+
+ans1 = []
+ans2 = []
+ans3 = []
+ans4  = []
+
+#program
+
+def histogram(): # This function is for the histogram (Part 2) and List [Extension] Part 3.
+    global ProgressList, MTrailerList, RetrieverList,ExcludedList, i
+
+    #Histogram , List (extension) 
+    print("-"*70)
+
+    print("Histogram")
+    print("Progress ",len(ProgressList),"  : ",len(ProgressList)*"*")
+    print("Trailer  ",len(MTrailerList),"  : ",len(MTrailerList)*"*")
+    print("Retriever",len(RetrieverList),"  : ",len(RetrieverList)*"*")
+    print("Excluded ",len(ExcludedList),"  : ",len(ExcludedList)*"*")
+    print("")
+    print(len(ProgressList) + len(MTrailerList) + len(RetrieverList) + len(ExcludedList), "outcome in total")
+    print("-"*70)
+
+    #Adding the part 2
+    print("Part 2: \n")
+    if len(ProgressList) != 0:
+        print("Progress - ", *ProgressList)
+    if len(MTrailerList) != 0:
+        print("Progress (module trailer) - ", *MTrailerList)
+    if len(RetrieverList) != 0:
+        print("Module retriever - ",*RetrieverList)
+    if len(ExcludedList) != 0:
+        print("Exclude - ",*ExcludedList)
+    print("-"*70)
+
+
+
+    
+
+
+def text_file():  # This function is for generate the text file to the Histogram.
+    global  ans1,ans2,ans3,ans4, ProgressList,MTrailerList,RetrieverList,ExcludedList, file_object
+    file_object = open("progression_data.txt","w")
+    if len(ProgressList) != 0:
+        file_object.write("Progress - "+str(ProgressList)+'\n')
+
+    if len(MTrailerList) != 0:
+        file_object.write('Progress (module trailer) - '+str(MTrailerList)+'\n')
+
+    if len(RetrieverList) != 0:
+        file_object.write('Module retriever - '+str(RetrieverList)+'\n')
+
+    if len(ExcludedList) != 0:
+        file_object.write('Excluded - '+str(ExcludedList)+'\n')
+    file_object.close()
+
+
+    file_object = open("progression_data.txt","r") 
+    print(file_object.read())
+
+    
+def grading():  # This function hold the grading system
+    
+    global credit_defer,total,credit_fail,credit_pass, ProgressList, MTrailerList, RetrieverList,ExcludedList
+    if credit_pass == 120:
+        print("Progress\n") 
+        ProgressList.append([credit_pass,credit_defer,credit_fail])
+        
+    elif credit_pass == 100:
+        print("Progress (module trailer)\n")
+        MTrailerList.append([credit_pass,credit_defer,credit_fail])
+    
+    elif credit_pass == 80:
+        print("Module retriever\n")
+        RetrieverList.append([credit_pass,credit_defer,credit_fail])
+        
+    elif credit_pass == 60:
+        print("Module retriever\n")
+        RetrieverList.append([credit_pass,credit_defer,credit_fail])
+        
+    elif credit_pass == 40 and credit_fail != 80:
+        print("Module retriever\n")
+        RetrieverList.append([credit_pass,credit_defer,credit_fail])
+        
+    elif credit_pass == 40 and credit_fail == 80:
+        print("Exclude\n")
+        ExcludedList.append([credit_pass,credit_defer,credit_fail])
+        
+    elif credit_pass == 20 and credit_fail <= 60:
+        print("Module retriever\n")
+        RetrieverList.append([credit_pass,credit_defer,credit_fail])
+        
+    elif credit_pass == 20 and credit_fail >= 80:
+        print("Exclude\n")
+        ExcludedList.append([credit_pass,credit_defer,credit_fail])
+        
+    elif credit_pass == 0 and credit_fail <= 60:
+        print("Module retriever\n")
+        RetrieverList.append([credit_pass,credit_defer,credit_fail])
+        
+    elif credit_pass == 0 and credit_fail >= 80:
+        print("Exclude\n")
+        ExcludedList.append([credit_pass,credit_defer,credit_fail])
+    
+        
+
+def asking_inputs():  # This function ask the Student ID's and pass marks from the user
+
+    global credit_defer, credit_fail,credit_pass,total
+    while True:
+        while True:     # Input the credit pass
+            try:
+                print("")
+                credit_pass = int(input("Please enter your credits at pass: "))
+                if credit_pass not in credit_range:
+                    print("Out of range.")
+                    continue
+                else:
+                    break
+            except ValueError:
+                print("Integer required")
+
+        while True:     # Input the defer credit 
+            try:    
+                credit_defer = int(input("Please enter your credit at defer: "))
+                if credit_defer not in credit_range:
+                    print("Out of range.")
+                    continue
+                else:
+                    break
+            except ValueError:
+                print("Integer required")
+
+        while True:     # Input the fail credit 
+            try:
+                credit_fail = int(input("Please enter your credit at fail: "))
+                if credit_fail not in credit_range:
+                    print("Out of range.")
+                    continue
+                else:
+                    break
+            except ValueError:
+                print("Integer required")
+
+        total = credit_pass + credit_defer + credit_fail
+
+        if total == 120:
+            grading()
+            break
+        else:
+            print("Total Incorrect ")
+            continue
+        
+
+def ending():    #This function controls wether the user wants to continue the program or stop the program and print the dictionary part 
+    while True:
+        try:
+            print("")
+            print("Would you like to enter another set of data? ")
+            ans=str(input("Enter 'y' for yes or 'q' to quit and view results:"))
+            if ans =="y":
+                asking_inputs()
+                continue
+            
+            elif ans == "q":
+                histogram()
+                break
+            else:
+                print("Invalid Input")
+                continue
+        except:
+            print("Inalid Input.")
+            continue
+asking_inputs()
+ending()
+
+# References
+# 1.Lecture materials
+# 2.w3schools Python: https://www.w3schools.com/python/
---
+++ Student_Dictionary_1998814.py
@@ -0,0 +1,176 @@
+# I declare that my work contains no examples of misconduct, such as plagiarism, or collusion. 
+# Any code taken from other sources is referenced within my code solution.  
+  
+# Student ID: 20220218 | W1998814          # Date:  2023/04/20
+
+# variables
+credit_range = [0,20,40,60,80,100,120]
+credit_pass=0
+credit_defer=0
+credit_fail=0
+
+ans = 0
+progress_count = 0
+module_trailer_count = 0
+module_retriever_count = 0
+exclude_count = 0
+total_outcomes = 0
+file_object = 0
+
+#lists
+ProgressList = []
+MTrailerList = []
+RetrieverList = []
+ExcludedList = []
+student_ids = []
+user_student_id = ''
+
+#Dictionary
+dictionary_list = {}
+
+
+
+def dictionary():  # This method is used to print the data sets in the dictionary, using a for loop.
+    print('')
+    print("part 4: \n")
+    print("-"*70)
+    print(" "*30,"Dictionary")
+    for x, y in dictionary_list.items():
+        print(x, ":", y)
+    print('')
+    print("-"*70)
+
+def grading():  # This function hold the grading system
+
+    global credit_defer,total,credit_fail,credit_pass, ProgressList, MTrailerList, RetrieverList,ExcludedList
+    if credit_pass == 120:
+        print("Progress\n") 
+        dictionary_list[user_student_id]=f"Progress - {credit_pass,credit_defer,credit_fail}"
+        ProgressList.append([credit_pass, credit_defer, credit_fail])
+
+    elif credit_pass == 100:
+        print("Progress (module trailer)\n")
+        dictionary_list[user_student_id] = f"Module trailer - {credit_pass, credit_defer, credit_fail}"
+        MTrailerList.append([credit_pass, credit_defer, credit_fail])
+    
+    elif credit_pass == 80:
+        print("Module retriever\n")
+        dictionary_list[user_student_id] = f"Module retriever - {credit_pass, credit_defer, credit_fail}"
+        RetrieverList.append([credit_pass, credit_defer, credit_fail])
+        
+    elif credit_pass == 60:
+        print("Module retriever\n")
+        dictionary_list[user_student_id] = f"Module retriever - {credit_pass, credit_defer, credit_fail}"
+        RetrieverList.append([credit_pass, credit_defer, credit_fail])
+        
+    elif credit_pass == 40 and credit_fail != 80:
+        print("Module retriever\n")
+        dictionary_list[user_student_id] = f"Module retriever - {credit_pass, credit_defer, credit_fail}"
+        RetrieverList.append([credit_pass, credit_defer, credit_fail])
+        
+    elif credit_pass == 40 and credit_fail == 80:
+        print("Exclude\n")
+        dictionary_list[user_student_id] = f"Exclude - {credit_pass, credit_defer, credit_fail}"
+        ExcludedList.append([credit_pass, credit_defer, credit_fail])
+        
+    elif credit_pass == 20 and credit_fail <= 60:
+        print("Module retriever\n")
+        dictionary_list[user_student_id] = f"Module retriever - {credit_pass, credit_defer, credit_fail}"
+        RetrieverList.append([credit_pass, credit_defer, credit_fail])
+        
+    elif credit_pass == 20 and credit_fail >= 80:
+        print("Exclude\n")
+        dictionary_list[user_student_id] = f"Exclude - {credit_pass, credit_defer, credit_fail}"
+        ExcludedList.append([credit_pass, credit_defer, credit_fail])
+        
+    elif credit_pass == 0 and credit_fail <= 60:
+        print("Module retriever\n")
+        dictionary_list[user_student_id] = f"Module retriever - {credit_pass, credit_defer, credit_fail}"
+        RetrieverList.append([credit_pass, credit_defer, credit_fail])
+        
+    elif credit_pass == 0 and credit_fail >= 80:
+        print("Exclude\n")
+        dictionary_list[user_student_id] = f"Exclude - {credit_pass, credit_defer, credit_fail}"
+        ExcludedList.append([credit_pass, credit_defer, credit_fail])
+
+
+def asking_inputs(): # This function ask the Student ID's and pass marks from the user
+
+    global credit_defer,credit_fail,credit_pass,total,student_ids, user_student_id
+    while True:
+        while True:  #Input the student ID 
+            try:
+                user_student_id = str(input("Please enter your student ID : "))
+                student_ids.append(user_student_id)
+                check_clone_id = student_ids.count(user_student_id)
+                if check_clone_id > 1:
+                    print("You have already entered this student id, Please try a different ID!")
+                    continue
+                else:
+                    break
+            except ValueError:
+                continue
+
+        while True:  # Input the credit pass 
+            try:
+                #print("")
+                credit_pass = int(input("Please enter your credits at pass: "))
+                if credit_pass not in credit_range:
+                    print("Out of range.")
+                    continue
+                else:
+                    break
+            except ValueError:
+                print("Integer required")
+
+        while True:  # Input the defer credit 
+            try:    
+                credit_defer = int(input("Please enter your credit at defer: "))
+                if credit_defer not in credit_range:
+                    print("Out of range.")
+                    continue
+                else:
+                    break
+            except ValueError:
+                print("Integer required")
+
+        while True:   # Input the fail credit 
+            try:
+                credit_fail = int(input("Please enter your credit at fail: "))
+                if credit_fail not in credit_range:
+                    print("Out of range.")
+                    continue
+                else:
+                    break
+            except ValueError:
+                print("Integer required")
+
+        total = credit_pass + credit_defer + credit_fail
+        
+        if total == 120:
+            grading()
+            break
+        else:
+            print("Incorrect Total, Try Again!")
+            continue
+        
+
+def ending():    #This function controls wether the user wants to continue the program or stop the program and print the dictionary part 
+    while(True):
+            print("")
+            print("Would you like to enter another set of data? ")
+            ans=str(input("Enter 'y' for yes or 'q' to quit and view results:"))
+            if ans =="y":
+                asking_inputs()
+                continue
+            
+            elif ans == "q":
+                #histogram()
+                dictionary()
+                break
+asking_inputs()
+ending()
+
+# References
+# 1.Lecture materials
+# 2.w3schools Python: https://www.w3schools.com/python/
